dbf39ee7703a181ee7f61a241e6a22c348b8a3a8
gabrydea25 <gabrydea25@gmail.com>
2026-02-24 20:09:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wheel = document.getElementById("wheel");
 const btn = document.getElementById("spinBtn");
 const result = document.getElementById("result");
+const bgMusic = document.getElementById("bgMusic");
+const tickSound = document.getElementById("tickSound");
 
 const options = [
   "Decidi un evento",
@@ -9,13 +11,31 @@ const options = [
   "Niente"
 ];
 
+let spinning = false;
+let tickInterval;
+
 btn.onclick = function() {
+
+  if (spinning) return;
+
+  spinning = true;
+
+  bgMusic.volume = 0.3;
+  bgMusic.play();
+
   let randomIndex = Math.floor(Math.random() * options.length);
   let degrees = 360 * 5 + (randomIndex * 90);
 
   wheel.style.transform = "rotate(-" + degrees + "deg)";
 
+  tickInterval = setInterval(() => {
+    tickSound.currentTime = 0;
+    tickSound.play();
+  }, 150);
+
   setTimeout(() => {
+    clearInterval(tickInterval);
     result.innerText = "Hai ottenuto: " + options[randomIndex];
+    spinning = false;
   }, 4000);
 };
