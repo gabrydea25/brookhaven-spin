940a2f6e3b1f30396a64a181d7ed1e22b72b5073
gabrydea25 <gabrydea25@gmail.com>
2026-02-24 19:07:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const wheel = document.getElementById("wheel");
+const btn = document.getElementById("spinBtn");
+const result = document.getElementById("result");
 
+const options = [
+  "Decidi un evento",
+  "Decidere il tema di una giornata",
+  "Pubblicizzare il tuo canale TikTok",
+  "Niente"
+];
+
+btn.onclick = function() {
+  let randomIndex = Math.floor(Math.random() * options.length);
+  let degrees = 360 * 5 + (randomIndex * 90);
+
+  wheel.style.transform = "rotate(-" + degrees + "deg)";
+
+  setTimeout(() => {
+    result.innerText = "Hai ottenuto: " + options[randomIndex];
+  }, 4000);
+};
